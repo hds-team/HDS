12af923b7b0bbce88fae27c470a5d44cb4c7ed7c
sososaity <pakorn_traipan@icloud.com>
2015-08-30 20:38:43 +0700

    แก้หน้าวิว position
    แก้วิวขี่กัน
    เพิ่ม comment
    แก้ part ของ menu

--- application/views/HDS/admin/position/v_position.php
+++ application/views/HDS/admin/position/v_position.php
@@ -1,109 +1,31 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<!-- Viewport metatags -->
-<meta name="HandheldFriendly" content="true" />
-<meta name="MobileOptimized" content="320" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
-<!-- iOS webapp metatags -->
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-
-<!-- iOS webapp icons -->
-<link rel="apple-touch-icon-precomposed" href="images/touch-icon-iphone.png" />
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/touch-icon-ipad.png" />
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/touch-icon-retina.png" />
-
-<!-- CSS Reset -->
-<link rel="stylesheet" type="text/css" href="css/reset.css" media="screen" />
-<!--  Fluid Grid System -->
-<link rel="stylesheet" type="text/css" href="css/fluid.css" media="screen" />
-<!-- Theme Stylesheet -->
-<link rel="stylesheet" type="text/css" href="css/dandelion.theme.css" media="screen" />
-<!--  Main Stylesheet -->
-<link rel="stylesheet" type="text/css" href="css/dandelion.css" media="screen" />
-<!-- Demo Stylesheet -->
-<link rel="stylesheet" type="text/css" href="css/demo.css" media="screen" />
-
-<!-- jQuery JavaScript File -->
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-
-<!-- jQuery-UI JavaScript Files -->
-<script type="text/javascript" src="jui/js/jquery-ui-1.8.20.min.js"></script>
-<script type="text/javascript" src="jui/js/jquery.ui.timepicker.min.js"></script>
-<script type="text/javascript" src="jui/js/jquery.ui.touch-punch.min.js"></script>
-<link rel="stylesheet" type="text/css" href="jui/css/jquery.ui.all.css" media="screen" />
-
-<!-- Plugin Files -->
-
-<!-- FileInput Plugin -->
-<script type="text/javascript" src="js/jquery.fileinput.js"></script>
-<!-- Placeholder Plugin -->
-<script type="text/javascript" src="js/jquery.placeholder.js"></script>
-<!-- Mousewheel Plugin -->
-<script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
-<!-- Scrollbar Plugin -->
-<script type="text/javascript" src="js/jquery.tinyscrollbar.min.js"></script>
-<!-- Tooltips Plugin -->
-<script type="text/javascript" src="plugins/tipsy/jquery.tipsy-min.js"></script>
-<link rel="stylesheet" href="plugins/tipsy/tipsy.css" />
-
-<!-- Validation Plugin -->
-<script type="text/javascript" src="plugins/validate/jquery.validate.min.js"></script>
-
-<!-- Chosen Plugin -->
-<script type="text/javascript" src="plugins/chosen/chosen.jquery.min.js"></script>
-<link rel="stylesheet" href="plugins/chosen/chosen.css" media="screen" />
-
-<!-- Demo JavaScript Files -->
-<script type="text/javascript" src="js/demo/demo.validation.js"></script>
-
-<!-- Core JavaScript Files -->
-<script type="text/javascript" src="js/core/dandelion.core.js"></script>
-
-<!-- Customizer JavaScript File (remove if not needed) -->
-<script type="text/javascript" src="js/core/dandelion.customizer.js"></script>
-
-<title>Dandelion Admin - Form Validation</title>
-
-</head>
-
-<body>
-	<div class="grid_4">
-		<div class="da-panel collapsible">
-			<div class="da-panel-header">
-				<span class="da-panel-title">
-					<img src="images/icons/black/16/list.png" alt="">
-						กำหนดสิทธิผู้ใช้ระบบ
-				</span>
-			<span class="da-panel-toggler"></span></div>
-			<div class="da-panel-content">
-				<table class="da-table">
-					<thead>
-						<tr>
-							<th><center>ลำดับ</center></th>
-							<th><center>รายชื่อ</center></th>
-							<th><center>เจ้าหน้าที่</center></th>
-							<th><center>นักพัฒนาระบบ</center></th>
-							<th><center>ผู้ดูแลระบบ</center></th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr class="odd">
-							<td><center>1</center></td>
-							<td><center>นาย ธนภัทร แก้ววิชิต</center></td>
-							<th><center><input type="radio" name="gender"></li></center></th></td>
-							<th><center><input type="radio" name="gender"></li></center></th></td>
-							<th><center><input type="radio" name="gender"></li></center></th></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>   
-</body>
-</html>
+<div class="da-panel collapsible">
+	<div class="da-panel-header">
+		<span class="da-panel-title">
+			<img src="<?php echo base_url('images/icons/black/16/list.png'); ?>" alt="">
+				กำหนดสิทธิผู้ใช้ระบบ
+		</span>
+		<span class="da-panel-toggler"></span>
+	</div><!-- da-panel-header -->
+	<div class="da-panel-content">
+		<table class="da-table">
+			<thead>
+				<tr>
+					<th><center>ลำดับ</center></th>
+					<th><center>รายชื่อ</center></th>
+					<th><center>เจ้าหน้าที่</center></th>
+					<th><center>นักพัฒนาระบบ</center></th>
+					<th><center>ผู้ดูแลระบบ</center></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="odd">
+					<td><center>1</center></td>
+					<td><center>นาย ธนภัทร แก้ววิชิต</center></td>
+					<td><center><input type="radio" name="gender"></li></center></th></td>
+					<td><center><input type="radio" name="gender"></li></center></th></td>
+					<td><center><input type="radio" name="gender"></li></center></th></td>
+				</tr>
+			</tbody>
+		</table>
+	</div><!-- da-panel-content -->
+</div>
