47c022abee4507b7818bc273b52e1f2ffcd20e83
kimbonbon <champ.black@hotmail.com>
2015-08-30 20:54:48 +0700

    แก้ไข view
    แก้ไข view ส่วนตารางและเพิ่มปุ่มsubmit

--- application/views/HDS/admin/position/v_position.php
+++ application/views/HDS/admin/position/v_position.php
@@ -1,31 +1,37 @@
-<div class="da-panel collapsible">
+<div class="da-panel">
 	<div class="da-panel-header">
 		<span class="da-panel-title">
 			<img src="<?php echo base_url('images/icons/black/16/list.png'); ?>" alt="">
 				กำหนดสิทธิผู้ใช้ระบบ
 		</span>
-		<span class="da-panel-toggler"></span>
 	</div><!-- da-panel-header -->
 	<div class="da-panel-content">
-		<table class="da-table">
-			<thead>
-				<tr>
-					<th><center>ลำดับ</center></th>
-					<th><center>รายชื่อ</center></th>
-					<th><center>เจ้าหน้าที่</center></th>
-					<th><center>นักพัฒนาระบบ</center></th>
-					<th><center>ผู้ดูแลระบบ</center></th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="odd">
-					<td><center>1</center></td>
-					<td><center>นาย ธนภัทร แก้ววิชิต</center></td>
-					<td><center><input type="radio" name="gender"></li></center></th></td>
-					<td><center><input type="radio" name="gender"></li></center></th></td>
-					<td><center><input type="radio" name="gender"></li></center></th></td>
-				</tr>
-			</tbody>
-		</table>
+		<form class="da-form">
+			<table class="da-table">
+				<thead>
+					<tr>
+						<th><center>ลำดับ</center></th>
+						<th><center>รายชื่อ</center></th>
+						<th><center>เจ้าหน้าที่</center></th>
+						<th><center>นักพัฒนาระบบ</center></th>
+						<th><center>ผู้ดูแลระบบ</center></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="odd">
+						<td><center>1</center></td>
+						<td><center>นาย ธนภัทร แก้ววิชิต</center></td>
+						<td><center><input type="radio" name="gender"></li></center></th></td>
+						<td><center><input type="radio" name="gender"></li></center></th></td>
+						<td><center><input type="radio" name="gender"></li></center></th></td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="da-button-row">
+				<input type="submit" value="Submit" class="da-button green">
+			</div><!-- da-button-row -->
+		</form>
 	</div><!-- da-panel-content -->
-</div>
+</div><!-- da-panel -->
+
+
