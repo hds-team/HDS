e2dc3cf651f166c0a144e3389602bb0a6c98034c
kimbonbon <champ.black@hotmail.com>
2015-09-03 15:46:52 +0700

    แก้ไข model
    แก้ไข model

--- application/controllers/HDS/admin_part/position.php
+++ application/controllers/HDS/admin_part/position.php
@@ -1,5 +1,7 @@
 <?php
-
+	//----------------LOAD MODEL
+	$this->load->model('HDS/admin/position/m_position');
+	$data['query']=$this->m_position->get_all();
 	//----------------LOAD VIEW
 	$data['content'] = $this->hds_output('admin/position/v_position', NULL, TRUE);
 	$this->layout_output($data);
--- application/models/HDS/admin/position/m_position.php
+++ application/models/HDS/admin/position/m_position.php
@@ -1,14 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require(dirname(__FILE__)."../../HDS_Model.php");
-class M_dynamic extends HDS_Model{
-	
-	//<!------get_all ums------>
+//require(dirname(__FILE__)."../../../HDS_Model.php");
+class M_position extends CI_Model{
+	public function __construct(){
+		parent::__construct();
+		$this->hds = $this->load->database('hds', TRUE);
+		$this->ums = $this->load->database('ums', TRUE);	
+	}
+	//--------get_all ums----------
 	public function get_all(){
 		$this->ums
 		->select('*')
 		->from('umuser');
-		return $this->umuser->get();
+		return $this->ums->get();
 	}
-
-
 }
