55fcaf3a252c8087eebbfda62a8a11cfbaa53738
fifaprakarn <se56160137@gmail.com>
2015-09-08 17:28:44 +0700

    แก้ไข model
    function show_check

--- application/models/HDS/screening/check/m_check.php
+++ application/models/HDS/screening/check/m_check.php
@@ -7,7 +7,7 @@ class M_check extends CI_Model{
 		$this->hds = $this->load->database('hds', TRUE);
 		$this->ums = $this->load->database('ums', TRUE);
 	}
-	public function show_check($rq_sys_id)
+	public function show_check($sys_id)
 	{
 		$sql = "SELECT * FROM hds_request
 		INNER JOIN hds_category
@@ -16,7 +16,7 @@ class M_check extends CI_Model{
 		ON hds_request.rq_st_id=hds_status.st_id
 		where hds_request.rq_sys_id = ?
 		";
-		$query = $this->hds->query($sql, array($this->rq_sys_id));
+		$query = $this->hds->query($sql, array($sys_id));
 		//$this->hds
 		//->select('*')
 		//->from('hds_category');
