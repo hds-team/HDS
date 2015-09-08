d6ebdd20c6757ccb2fce7df9b3506f025eb45f39
Modtanoiizz <mod_2660@hotmail.com>
2015-09-08 14:12:09 +0700

    $sys_id

--- application/models/HDS/dev_work/ongoing/m_ongoing.php
+++ application/models/HDS/dev_work/ongoing/m_ongoing.php
@@ -6,7 +6,7 @@ class M_ongoing extends CI_Model{
 		$this->hds = $this->load->database('hds', TRUE);
 		$this->ums = $this->load->database('ums', TRUE);
 	}
-	public function get_request() //get request and  status 
+	public function get_request($sys_id) //get request and  status 
 	{
 		$this->hds
 		->select('*')
@@ -15,7 +15,7 @@ class M_ongoing extends CI_Model{
 		->join('hds_v1.hds_kind', 'hds_kind.kn_id = hds_request.rq_kn_id', 'inner') //inner kind and request
 		->join('hds_v1.hds_status', 'hds_status.st_id = hds_request.rq_st_id', 'inner') //inner join status
 		->join('ums.umuser', 'umuser.UsID = hds_request.rq_mb_id', 'inner') //join ums
-		->where('hds_request.rq_sys_id',10) //system's ums
+		->where('hds_request.rq_sys_id',$sys_id) //system's ums
 		->where('hds_request.rq_st_id',4); //status sending
 		$query = $this->hds->get();
 		return $query;
