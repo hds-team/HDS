4af8b84838630b8323d57f7c876915bed23d6335
Joevanjoke <chiichha.vanjoke@gmail.com>
2015-09-09 14:59:09 +0700

    แก้ไขตัวแปรที่ส่งค่าจากฐานข้อมูลให้ถูกต้อง

--- application/views/HDS/dev_work/approve/v_approve.php
+++ application/views/HDS/dev_work/approve/v_approve.php
@@ -17,11 +17,15 @@
         <tbody>
             <td><center><?php echo $index+1; ?></center></td>
             <td><?php echo $row->rq_subject; ?></td>
-            <td><?php echo $row->rp_date;?></td>
+            <td><?php echo $row->rq_date;?></td>
             <td><?php echo $row->ct_name; ?></td>
             <td><?php echo $row->kn_name; ?></td>
             <td><?php echo $row->UsName; ?></td>
-            <td><input type="submit" value="รับทราบ" class="da-button green"> </td>
+            <td>
+                <a href = "<?php echo base_url('index.php/HDS/dev_work/update_approve/'.$row->rq_id.'/'.$row->st_id); ?>" />
+                    <input type="submit" value="รับทราบ" class="da-button green"> 
+                </a>
+            </td>
         </tbody>
         <?php 
             $index++;
