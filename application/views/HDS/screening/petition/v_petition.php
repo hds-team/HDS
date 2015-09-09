81e429b7fc435cef313bf378414c0acd39e45c18
natkamonlala <natkamon16@hotmail.com>
2015-09-09 10:42:16 +0700

    V_petition Part
    V_petition Part
    + Link of Subject detail

--- application/views/HDS/screening/petition/v_petition.php
+++ application/views/HDS/screening/petition/v_petition.php
@@ -17,7 +17,7 @@
 				?>
                 <tr class="odd">
                 	<td><center> <?php echo $index++; ?> </center></td>
-                    <td><?php echo $row->rq_subject; ?></td>
+					<td><a href="<?php echo base_url('index.php/HDS/reply/detail_sys/'.$row->rq_id); ?>" target="_blank"><?php echo $row->rq_subject; ?></a></td>
                     <td><center><?php echo $row->rq_date; ?></center></td>
                     <td><center><?php echo $row->ct_name; ?></center></td>
                     <td><center><?php echo $row->UsName; ?></center></td>
