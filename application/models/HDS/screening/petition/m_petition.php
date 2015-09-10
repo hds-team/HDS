171e158767f6bb9d8242607d09202501d8466e6d
sososaity <pakorn_traipan@icloud.com>
2015-09-10 10:09:17 +0700

    แก้ไข m_pettition
    จอยโดนใช้คีย์ในการจอยผิด

--- application/models/HDS/screening/petition/m_petition.php
+++ application/models/HDS/screening/petition/m_petition.php
@@ -12,11 +12,11 @@ class M_petition extends CI_Model{
 		$this->hds
 			->select('*')
 			->from('hds_request')
-			->join('hds_category','hds_request.rq_id = hds_category.ct_id','inner')
-			->join('hds_status','hds_request.rq_id = hds_status.st_id','inner')
+			->join('hds_category','hds_request.rq_ct_id = hds_category.ct_id','inner')
+			->join('hds_status','hds_request.rq_st_id = hds_status.st_id','inner')
 			->join('ums.umuser','hds_request.rq_mb_id = ums.umuser.UsID','inner')
-			->where('rq_sys_id',$sys_id)
-			->where('hds_request.rq_st_id',1);
+			->where('hds_request.rq_sys_id', $sys_id)
+			->where('hds_request.rq_st_id', 1);
 			
 		return $this->hds->get();
 	}
