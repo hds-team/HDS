05b5108b4f4f57b9015237426c28a53a0be7b0d7
sososaity <pakorn_traipan@icloud.com>
2015-09-10 10:10:42 +0700

    ลบ ส่วนของการเทส module check

--- application/controllers/HDS/screening_part/check.php
+++ application/controllers/HDS/screening_part/check.php
@@ -1,11 +1,11 @@
 ﻿<?php
 	$this->load->model("HDS/screening/check/m_check");
-	echo "dddd"."<br>";
 	$data['query'] = $this->m_check->show_check($sys_id)->result_array();
-	echo "dddd"."<br>";
+	/*
 	foreach($data['query'] as $key => $ch)
 	{	
 			echo $ch['ct_name']."<br>";
 	}
+	*/
 	$view = $this->hds_output("screening/check/v_check", $data,true);
 ?>
