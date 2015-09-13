f296475044df24e30414145bcd9d44d16a6404d9
Joevanjoke <chiichha.vanjoke@gmail.com>
2015-09-13 15:52:29 +0700

    ประกาศตัวแปร sys_id

--- application/controllers/HDS/dev_work_part/approve.php
+++ application/controllers/HDS/dev_work_part/approve.php
@@ -1,5 +1,6 @@
 <?php
 	$this->load->model('HDS/dev_work/approve/m_approve');
-	$data['approve'] = $this->m_approve->get_report();
+	$data['approve'] = $this->m_approve->get_report($sys_id);
+	$data['sys_id'] = $sys_id;
 	$view = $this->hds_output('dev_work/approve/v_approve', $data, true);
 ?>
