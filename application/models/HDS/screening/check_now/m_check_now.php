bc3e0ee7ea22768c7ad05cc2bd3415c684d517e6
Nomklong <se56160103@gmail.com>
2015-09-16 11:12:09 +0700

    แก้ไข select

--- application/models/HDS/screening/check_now/m_check_now.php
+++ application/models/HDS/screening/check_now/m_check_now.php
@@ -14,7 +14,7 @@ class M_check_now extends CI_Model{
 		->join('hds_category','hds_request.rq_ct_id=hds_category.ct_id' ,'inner')
 		->join('ums.umuser', 'hds_request.rq_mb_id=ums.umuser.UsID', 'inner')
 		->join('hds_status','hds_request.rq_st_id=hds_status.st_id','inner')
-		->where('rq_sys_id', $sys_id);
+		->where('rq_sys_id', $sys_id,'hds_status.st_id!=8');
 		return $this->hds->get();
 	} //check_now_require
 } //class m_check_now
