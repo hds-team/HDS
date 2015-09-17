0b97fc9a2729a699500752049debdca05d09dee0
Joevanjoke <chiichha.vanjoke@gmail.com>
2015-09-17 11:56:31 +0700

    แก้ไขการอัพเดทสถานะ

--- application/controllers/HDS/dev_work_part/update_approve.php
+++ application/controllers/HDS/dev_work_part/update_approve.php
@@ -1,6 +1,6 @@
 <?php 
 	$this->load->model('HDS/dev_work/approve/M_approve');
-	$data['rq_st_id'] = 7;
+	$data['rq_st_id'] = $st_id;
 	$this->m_dynamic->update('hds_request','rq_id',$rq_id,$data);
 	//echo $data['rq_st_id'];
 	redirect('HDS/dev_work/index/'.$sys_id.'#approve');
