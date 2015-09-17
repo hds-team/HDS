c8658bdd70c25a15500ce3abfae8212b9d5e7b3c
Joevanjoke <chiichha.vanjoke@gmail.com>
2015-09-17 11:55:36 +0700

    ปรับเปลี่ยนหน้าอินเตอร์เฟสให้ดูดีขึ้น

--- application/views/HDS/dev_work/approve/v_approve.php
+++ application/views/HDS/dev_work/approve/v_approve.php
@@ -2,13 +2,13 @@
     <table id = "da-ex-datatable-numberpaging" class = "da-table">
         <thead>
 			<tr role="row"> <!-- row -->
-				<th>ลำดับ</th>
-				<th>หัวเรื่อง</th>
-				<th>วันที่</th>
-				<th>ประเภท</th>
-				<th>หมวด</th>
-				<th>ผู้ส่ง</th>
-				<th>ดำเนินการ</th>
+				<th><center><B>ลำดับ</B></center></th>
+				<th><center><B>หัวเรื่อง</B></center></th>
+				<th><center><B>วันที่</B></center></th>
+				<th><center><B>ประเภท</B></center></th>
+				<th><center><B>หมวด</B></center></th>
+				<th><center><B>ผู้ส่ง</B></center></th>
+				<th><center><B>ดำเนินการ</B></center></th>
 			</tr>
         </thead>
         <tbody>
@@ -23,10 +23,26 @@
 				<td><?php echo $row->ct_name; ?></td>
 				<td><?php echo $row->kn_name; ?></td>
 				<td><?php echo $row->UsName; ?></td>
-				<td>
+				<td><center>
+					<?php 
+						if($row->rq_st_id == 6){
+							$row->st_id = 8;
+					?>
 					<a href = "<?php echo base_url('index.php/HDS/dev_work/update_approve/'.$row->rq_id.'/'.$row->st_id).'/'.$sys_id; ?>" />
 						<input type="submit" value="รับทราบ" class="da-button green"> 
 					</a>
+					<?php 
+						}
+						else{
+							$row->st_id = 3;
+					?>
+					<a href = "<?php echo base_url('index.php/HDS/dev_work/update_approve/'.$row->rq_id.'/'.$row->st_id).'/'.$sys_id; ?>" />
+						<input type="submit" value="แก้ไข" class="da-button blue">
+					</a>
+					<?php
+						}
+					?>
+					</center>
 				</td>
 			</tr>
 			<?php 
