b56f7d3ca90fe2f8d2ea796cae9c23cb5f247684
sososaity <pakorn_traipan@icloud.com>
2015-09-18 13:54:23 +0700

    Revert "Revert "แก้ไข function ลบ""
    This reverts commit 47f94244bff9df158fda75db6c25dde74aa7ac41.

--- application/controllers/HDS/fundamental_part/kind.php
+++ application/controllers/HDS/fundamental_part/kind.php
@@ -1,4 +1,5 @@
 <?php
-	$data['get_kind'] = $this->m_dynamic->get_all('hds_kind');
-	$view = $this->hds_output("fundamental/kind/v_kind", $data, true);
+	$this->load->model('HDS/fundamentnal/kind/m_kind');
+	$data['get_kind'] = $this->m_kind->get_kind();
+	$view = $this->hds_output('fundamental/kind/v_kind', $data, true);
 ?>
