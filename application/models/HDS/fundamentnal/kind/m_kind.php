ba42f84505bb713a921997707ab285f7b9e830c3
kaimook <muk_anonae@hotmail.com>
2015-09-22 14:01:16 +0700

    แก้ไข Model
    เพิ่มการจัดกลุ่ม group by kn_id ของตาราง hds_kind ในไฟล์ m_kind.php

--- application/models/HDS/fundamentnal/kind/m_kind.php
+++ application/models/HDS/fundamentnal/kind/m_kind.php
@@ -14,7 +14,8 @@ class M_kind extends CI_Model
 		$this->hds
 		->select('*')
 		->from('hds_kind')
-		->join('hds_request','hds_kind.kn_id = hds_request.rq_kn_id','left');
+		->join('hds_request','hds_kind.kn_id = hds_request.rq_kn_id','left')
+		->group_by('hds_kind.kn_id');
 		return $this->hds->get();
 	}
 }
