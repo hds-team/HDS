a6f297cef6f2d2ce1944fd3182e59116d69b20a9
Modtanoiizz <mod_2660@hotmail.com>
2015-09-23 14:30:04 +0700

    config sys_name by fifa
    -ส่วนของ dev_work ทั้งหมด

--- application/controllers/HDS/screening_part/update_check.php
+++ application/controllers/HDS/screening_part/update_check.php
@@ -1,5 +1,5 @@
 <?php
 	$data['rq_st_id'] = $st_id;
 	$this->m_dynamic->update('hds_request','rq_id',$rq_id,$data);
-	redirect('HDS/screening/index/'.$sys_id.'#check');
+	redirect($this->config->item('sys_name').'/screening/index/'.$sys_id.'#check');
 ?>
--- application/controllers/HDS/screening_part/update_petition_complete.php
+++ application/controllers/HDS/screening_part/update_petition_complete.php
@@ -2,5 +2,5 @@
 	$this->load->model('HDS/screening/petition/m_petition'); // lode m_petition from model part
 	$data['rq_st_id'] = 8; // set the value of id after press the button to 8
 	$this->m_dynamic->update('hds_request','rq_id',$rq_id,$data); // update the value after press button equal to $data['rq_st_id'] LIKE THIS >> 'table name','table id','value get from the view page','value that set equal to 8 (change to this value after press button)
-	redirect('HDS/screening/index/'.$sys_id.'#petition' ); // re page to the main screen
+	redirect($this->config->item('sys_name').'/screening/index/'.$sys_id.'#petition' ); // re page to the main screen
 ?>
