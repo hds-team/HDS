af592637085d5c961644729254eeded615714544
Modtanoiizz <mod_2660@hotmail.com>
2015-09-23 14:53:08 +0700

    save_log accept_pettition

--- application/controllers/HDS/screening_part/update_petition_accept.php
+++ application/controllers/HDS/screening_part/update_petition_accept.php
@@ -1,6 +1,8 @@
 <?php 
 	$this->load->model('HDS/screening/petition/m_petition'); // lode m_petition from Model part
 	$data['rq_st_id'] = 2; // set rq_st_id to 2 after press the button 
+	$st_id = $data['rq_st_id'];
+	$this->save_log( $st_id, $rq_id );
 	$this->m_dynamic->update('hds_request','rq_id',$rq_id,$data); // update value 'tqble name','id of table','value get from the View page','$data that set to 2 after press the button'
 	redirect($this->config->item('sys_name').'/screening/index/'.$sys_id.'#petition' ); // re the screen to index page
 	
