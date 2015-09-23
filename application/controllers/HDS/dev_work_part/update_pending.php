93037205b7371cd3673ab75bd973cc8dac6ab19e
Nomklong <se56160103@gmail.com>
2015-09-23 15:01:32 +0700

    เพิ่ม save log และ แก้ redirect
    ส่วนของ dev_work

--- application/controllers/HDS/dev_work_part/update_pending.php
+++ application/controllers/HDS/dev_work_part/update_pending.php
@@ -1,5 +1,13 @@
 <?php
-	$data['rq_st_id'] = $rq_st_id; 											//Creat $data['rq_st_id'] = $rq_st_id; Exp: 2 = 2
+	$data['rq_st_id'] = $rq_st_id;											//Creat $data['rq_st_id'] = $rq_st_id; Exp: 2 = 2
+	
+	//------------- Save Log -------------
+	
+	$this->save_log($rq_st_id,$rq_id);
+	
+	//----------- End Save Log ------------
+	
 	$this->m_dynamic->update('hds_request', 'rq_id', $rq_id, $data);		//Call model function update.
-	redirect('HDS/dev_work/index/'.$sys_id.'#pending');						//Redirect page.
+	$system_name = $this->config->item('sys_name');							// add sys_name to call system name
+	redirect($system_name.'/dev_work/index/'.$sys_id.'#pending');			//Redirect page.
 ?>
