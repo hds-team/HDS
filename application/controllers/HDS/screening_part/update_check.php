51caf27cfe8b7f4b6c94fab3b24878b91ec7a0b6
fifaprakarn <se56160137@gmail.com>
2015-09-23 22:50:35 +0700

    Save_log และ Config ในส่วน screening

--- application/controllers/HDS/screening_part/update_check.php
+++ application/controllers/HDS/screening_part/update_check.php
@@ -1,5 +1,6 @@
 <?php
 	$data['rq_st_id'] = $st_id;
+	$this->save_log( $st_id, $rq_id );
 	$this->m_dynamic->update('hds_request','rq_id',$rq_id,$data);
-	redirect('HDS/screening/index/'.$sys_id.'#check');
+	redirect($this->config->item($sys_name).'/screening/index/'.$sys_id.'#check');
 ?>
--- application/controllers/HDS/screening_part/update_petition_accept.php
+++ application/controllers/HDS/screening_part/update_petition_accept.php
@@ -1,7 +1,8 @@
 <?php 
 	$this->load->model('HDS/screening/petition/m_petition'); // lode m_petition from Model part
 	$data['rq_st_id'] = 2; // set rq_st_id to 2 after press the button 
+	$this->save_log( 2, $rq_id );
 	$this->m_dynamic->update('hds_request','rq_id',$rq_id,$data); // update value 'tqble name','id of table','value get from the View page','$data that set to 2 after press the button'
-	redirect('HDS/screening/index/'.$sys_id.'#petition' ); // re the screen to index page
+	redirect($this->config->item($sys_name).'/screening/index/'.$sys_id.'#petition' ); // re the screen to index page
 	
 ?>
