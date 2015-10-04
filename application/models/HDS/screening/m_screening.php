3d72ef8989b8bacacc43f56788cb729982182d59
sososaity <pakorn_traipan@icloud.com>
2015-10-04 18:48:36 +0700

    แก้ข้อผิดพลาก notification screening
    เกิดจากการ join ไม่ติด จึงแก้ไขให้ notification join ด้วย

--- application/models/HDS/screening/m_screening.php
+++ application/models/HDS/screening/m_screening.php
@@ -21,6 +21,10 @@ class M_screening extends CI_Model
 		$this->hds
 		->select(' rq_sys_id, COUNT(rq_sys_id) as total')
 		->from('hds_request')
+		->join('hds_category','hds_request.rq_ct_id = hds_category.ct_id','inner')
+		->join('hds_status','hds_request.rq_st_id = hds_status.st_id','inner')
+		->join($this->ums.'.umuser','hds_request.rq_mb_id = '.$this->ums.'.umuser.UsID','inner')
+		
 		->where('rq_st_id', 1)
 		->group_by('rq_sys_id');
 		return $this->hds->get();
