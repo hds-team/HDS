6306c3757e189a262032a1a5f4d7935396f91a02
Nomklong <se56160103@gmail.com>
2015-10-27 14:44:53 +0700

    history แก้ไข

--- application/controllers/HDS/history_part/history_finish.php
+++ application/controllers/HDS/history_part/history_finish.php
@@ -1,9 +1,12 @@
 ﻿<?php
-	$this->load->model('HDS/history/m_history');
+	$this->load->model($this->config->item('sys_name').'/history/m_history');
 	//------- Check show all
 	$this->session->userdata('UsID'); //show all
 	$data['query'] = $this->m_history->get_request(); //show only one system
 
 	//---------- LOAD VIEW
+	/*foreach($data["query"]->result() as $row){
+		echo $row->rq_st_id;
+	}*/
 	$data['content'] = $this->hds_output("history/history_finish", $data, True);
 ?>
--- application/models/HDS/history/m_history.php
+++ application/models/HDS/history/m_history.php
@@ -15,19 +15,17 @@ class M_history extends CI_Model
 		$this->ums = $this->config->item('UMS');
 	}
 
-	public function get_request() //get request and  status 
+	public function get_request() //get request and  status
 	{
 		$this->hds
 		->select('*')
-		->from($this->db_name.'.hds_request')
-		->join($this->db_name.'.hds_category', 'hds_category.ct_id = hds_request.rq_ct_id', 'inner') //inner category and request
-		->join($this->db_name.'.hds_kind', 'hds_kind.kn_id = hds_request.rq_kn_id', 'inner') //inner kind and request
-		->join($this->db_name.'.hds_status', 'hds_status.st_id = hds_request.rq_st_id', 'inner') //inner join status
-		->join($this->ums.'.umuser', 'umuser.UsID = hds_request.rq_mb_id', 'inner') //join ums
+		->from($this->db_name.'.hds_accept_log')
+		->join($this->db_name.'.hds_request', 'hds_accept_log.al_rq_id = hds_request.rq_id', 'inner') //inner hds_accept_log and request
+		->join($this->db_name.'.hds_status', 'hds_request.rq_st_id = hds_status.st_id', 'inner') //inner status and request
 		->join($this->ums.'.umsystem', 'umsystem.StID = hds_request.rq_sys_id', 'inner')
-		//->where('hds_request.rq_sys_id',$sys_id) //system's ums
-		->where('hds_request.rq_st_id',8) //status sending
-		->order_by('hds_request.rq_date', 'DESC');
+		->where('al_mb_id', $this->session->userdata('UsID')) //check session
+		->where('hds_accept_log.al_st_id', 4)
+		->where('hds_request.rq_st_id', 8);
 		$query = $this->hds->get();
 		return $query;
 	}
