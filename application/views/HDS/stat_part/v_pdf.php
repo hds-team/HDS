37a60f17bdee5af485923e9777c1d150d49872ad
natkamonlala <natkamon16@hotmail.com>
2015-11-08 18:27:19 +0700

    Export as PDF for finish  v_pdf

--- application/views/HDS/stat_part/v_pdf.php
+++ application/views/HDS/stat_part/v_pdf.php
@@ -1,30 +1,53 @@
 <html>
-<head>
-<title></title>
-</head>
 <body>
-<?php
-	//require('fpdf.php'); 
+<?php 
 	define('FPDF_FONTPATH','font/');
 	$this->load->library('fpdf');
+	$pdf=new PDF();
+	$pdf->SetMargins( 10,10,5 );
+	$pdf->AddPage(); 
+	/*$pdf->AddFont('angsa','','angsa.php');
+	$pdf->SetFont('angsa','',15);
+	$pdf->SetXY (10,30); */ //########Set All of Document########
 	
 	//ini_set("session.auto_start", 0);
-	$pdf=new PDF();
-	$pdf->SetMargins( 5,5,5 );
-	$pdf->AddPage();
-	$pdf->AddFont('angsa','','angsa.php');
-	$pdf->SetFont('angsa','',15);
-	$pdf->SetXY (10,50);
 	//mysql_query("SET NAMES 'tis620' ");
-	
-				$index=1;
+	//iconv('iso-8859-1', 'utf-8', $s);
+
+					/*foreach($query->result() as $row) //########est Export by foreach for each value########
+					{
+						//$pdf->Write(5,$row->rq_subject);
+						  $pdf->Write(8,iconv("UTF-8","TIS-620",$row->rq_subject));
+						//$pdf->Cell(25,8,iconv("UTF-8","TIS-620", $result["username"]),1,0,'L');
+					}*/
+					$pdf->AddFont('angsana','B','angsab.php');
+					$pdf->SetFont('angsana','B',16);
+					$pdf->Cell(15,7,iconv("UTF-8","TIS-620","ลำดับ"),1,0,'C');
+					$pdf->Cell(100,7,iconv("UTF-8","TIS-620","กิจกรรม"),1,0,'C');
+					$pdf->Cell(30,7,iconv("UTF-8","TIS-620", "วันที่"),1,0,'C');
+					$pdf->Cell(45,7,iconv("UTF-8","TIS-620", "หมายเหตุ"),1,0,'C');
+					$pdf->Ln();
+					$cnt = 0;
+					 
+					$pdf->AddFont('angsana','','angsa.php');
+					$pdf->SetFont('angsana','',16);
+					$index=1;
 					foreach($query->result() as $row)
 					{
-						$pdf->Write(5,$row->rq_subject);
+						$pdf->Cell(15,7,iconv("UTF-8","TIS-620",$index++),1,0,'C');
+						$pdf->Cell(100,7,iconv("UTF-8","TIS-620",$row->rq_subject),1,0,'L');
+						$pdf->Cell(30,7,iconv("UTF-8","TIS-620",$this->date_time->DateThai($row->rq_date)),1,0,'C');
+						$pdf->Cell(45,7,iconv("UTF-8","TIS-620",$row->ctr_value),1,0,'C');
+						$pdf->Ln(); 
+						if($cnt == 35)
+						{
+							$cnt = 0;
+							$pdf->AddPage();
+						}
 					}
 	
-	$pdf->SetFontSize(10);
-	$pdf->Write(5,'Congratulations! You have generated a PDF.');
+	/*$pdf->SetFontSize(10); 
+	$pdf->Write(5,'TEST Export some text.');*/ ########Test export normall text########
 	
 	ob_end_clean();
 	$pdf->Output("MyPDF/MyPDF.pdf","D");
@@ -34,55 +57,36 @@
 	} */
 
 	class PDF extends FPDF
-	{
+	{		
 		function Header(){
-			//$this->Image('thaicreate-logo.jpg',87,0,40);
+			if($this->PageNo() == 1 || $this->PageNo() != 1){ // Use for if want non-no of page number 1 [first page dont have a number -> use this !=1]
+			//$this->Image('thaicreate-logo.jpg',87,0,40);	
+			$this->SetTextColor(105, 105, 105);
 			$this->AddFont('angsa','','angsa.php');
-			$this->SetFont('angsa','',15);
-	 		$this->Cell(0,0,iconv( 'UTF-8','TIS-620','page '.$this->PageNo()),0,1,"R");
+			$this->SetFont('angsa','',13);
+			$this->Cell(0,0,iconv("UTF-8","TIS-620","โครงการดูแลบำรุงรักษาระบบสารสนเทศและระบบเครื่องแม่ข่ายคอมพิวเตอร์ ของวิทยาลัยการสาธารณสุขสิรินธร จังหวัดขอนแก่น "),0,0,'L');
+			$this->Cell(-5,0,iconv('UTF-8','TIS-620',''.$this->PageNo()),0,1,"R");
+			$this->Line(10,15,200,15);
 			$this->Ln(20);
+			}
 		}
 			
 		function Footer(){
+			if($this->PageNo() == 1){ 
+			$this->SetTextColor(105, 105, 105);
 			$this->AddFont('angsa','','angsa.php');
-			$this->SetFont('angsa','',10);
+			$this->SetFont('angsa','',13);
 			$this->SetY(-15);
-	 		$this->Cell(0,0,iconv( 'UTF-8','TIS-620','By... ไทยครีเอทดอทคอม'),0,1,"L");
-			$this->Cell(0,0,iconv( 'UTF-8','TIS-620','Date : '.date("Y-m-d")),0,1,"R");
+	 		//$this->Cell(0,0,iconv( 'UTF-8','TIS-620','test'),0,0,"L");
+			//$this->Cell(-5,0,iconv( 'UTF-8','TIS-620','Date : '.date("Y-m-d")),0,1,"R");
+			$this->Cell(190,0,iconv("UTF-8","TIS-620","ห้องปฏิบัติการวิจัยวิศวกรรมระบบสารสนเทศ คณะวิทยาการสารสนเทศ มหาวิทยาลัยบูรพา "),0,1,'R'); //-5 old value if not have line 2 of right
+			$this->Ln(5);
+			$this->Cell(190,0,iconv("UTF-8","TIS-620","วิทยาลัยการสาธารณสุขสิรินธร จังหวัดขอนแก่น สถาบันพระบรมราชชนก "),0,1,'R');
+			$this->Line(10,270,200,270);  
+			}
 		}
 	}
 ?>
-	<br>
-	<div>
-	<center><table border="1">
-	<thead>
-                <tr>
-                    <th style="width: 7%"><center><b>ลำดับ</b><center></th> 
-                    <th class="center"><b>กิจกรรม</b></th>
-                    <th style="width: 11%"><center><b>วันที่</b><center></th> 
-                    <th style="width: 20%"><center><b>หมายเหตุ</b><center></th> 
-                </tr>
-    </thead>
-	<tbody>
-				<?php 
-					$index=1;
-					foreach($query->result() as $row)
-					{
-				?> 
-					<tr>
-						<td><center><?php echo $index++; ?></center></td>
-						<td><center><?php echo $row->rq_subject; ?></center></td>
-						<td><center><?php echo $this->date_time->DateThai($row->rq_date)?></center></td>
-						<td><center><?php echo $row->ctr_value;?></center></td>
-					</tr>
-				<?php 
-					} 
-				?>
-    </tbody>
-	</table>
-	</center>
-	</div>
-	<br>
 </body>
 	PDF Created Click <a href="../MyPDF/MyPDF.pdf">here</a> to Download
 </html>
