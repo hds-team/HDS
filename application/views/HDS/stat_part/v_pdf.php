74e01c8749f41f9ba7159f3260858bfe6d90c107
natkamonlala <natkamon16@hotmail.com>
2015-11-08 21:17:53 +0700

    Cell -> Multicell
    Cell -> Multicell
    FIX_bug

--- application/views/HDS/stat_part/v_pdf.php
+++ application/views/HDS/stat_part/v_pdf.php
@@ -18,14 +18,14 @@
 					{
 						//$pdf->Write(5,$row->rq_subject);
 						  $pdf->Write(8,iconv("UTF-8","TIS-620",$row->rq_subject));
-						//$pdf->Cell(25,8,iconv("UTF-8","TIS-620", $result["username"]),1,0,'L');
+						//$pdf->Multicell(25,8,iconv("UTF-8","TIS-620", $result["username"]),1,0,'L');
 					}*/
 					$pdf->AddFont('angsana','B','angsab.php');
 					$pdf->SetFont('angsana','B',16);
-					$pdf->Cell(15,7,iconv("UTF-8","TIS-620","ลำดับ"),1,0,'C');
-					$pdf->Cell(100,7,iconv("UTF-8","TIS-620","กิจกรรม"),1,0,'C');
-					$pdf->Cell(30,7,iconv("UTF-8","TIS-620", "วันที่"),1,0,'C');
-					$pdf->Cell(45,7,iconv("UTF-8","TIS-620", "หมายเหตุ"),1,0,'C');
+					$pdf->Multicell(15,7,iconv("UTF-8","TIS-620","ลำดับ"),1,0,'C');
+					$pdf->Multicell(100,7,iconv("UTF-8","TIS-620","กิจกรรม"),1,0,'C');
+					$pdf->Multicell(30,7,iconv("UTF-8","TIS-620", "วันที่"),1,0,'C');
+					$pdf->Multicell(45,7,iconv("UTF-8","TIS-620", "หมายเหตุ"),1,0,'C');
 					$pdf->Ln();
 					$cnt = 0;
 					 
@@ -34,10 +34,10 @@
 					$index=1;
 					foreach($query->result() as $row)
 					{
-						$pdf->Cell(15,7,iconv("UTF-8","TIS-620",$index++),1,0,'C');
-						$pdf->Cell(100,7,iconv("UTF-8","TIS-620",$row->rq_subject),1,0,'L');
-						$pdf->Cell(30,7,iconv("UTF-8","TIS-620",$this->date_time->DateThai($row->rq_date)),1,0,'C');
-						$pdf->Cell(45,7,iconv("UTF-8","TIS-620",$row->ctr_value),1,0,'C');
+						$pdf->Multicell(15,7,iconv("UTF-8","TIS-620",$index++),1,0,'C');
+						$pdf->Multicell(100,7,iconv("UTF-8","TIS-620",$row->rq_subject),1,0,'L');
+						$pdf->Multicell(30,7,iconv("UTF-8","TIS-620",$this->date_time->DateThai($row->rq_date)),1,0,'C');
+						$pdf->Multicell(45,7,iconv("UTF-8","TIS-620",$row->ctr_value),1,0,'C');
 						$pdf->Ln(); 
 						if($cnt == 35)
 						{
@@ -53,7 +53,7 @@
 	$pdf->Output("MyPDF/MyPDF.pdf","D");
 	
 	/*for( $i=0;$i<=50;$i++ ){
-		$pdf->Cell(0,10,iconv( 'UTF-8','TIS-620','ไทยครีเอทดอทคอม '. $i),0,1,"C");
+		$pdf->Multicell(0,10,iconv( 'UTF-8','TIS-620','ไทยครีเอทดอทคอม '. $i),0,1,"C");
 	} */
 
 	class PDF extends FPDF
@@ -64,8 +64,8 @@
 			$this->SetTextColor(105, 105, 105);
 			$this->AddFont('angsa','','angsa.php');
 			$this->SetFont('angsa','',13);
-			$this->Cell(0,0,iconv("UTF-8","TIS-620","โครงการดูแลบำรุงรักษาระบบสารสนเทศและระบบเครื่องแม่ข่ายคอมพิวเตอร์ ของวิทยาลัยการสาธารณสุขสิรินธร จังหวัดขอนแก่น "),0,0,'L');
-			$this->Cell(-5,0,iconv('UTF-8','TIS-620',''.$this->PageNo()),0,1,"R");
+			$this->Multicell(0,0,iconv("UTF-8","TIS-620","โครงการดูแลบำรุงรักษาระบบสารสนเทศและระบบเครื่องแม่ข่ายคอมพิวเตอร์ ของวิทยาลัยการสาธารณสุขสิรินธร จังหวัดขอนแก่น "),0,0,'L');
+			$this->Multicell(-5,0,iconv('UTF-8','TIS-620',''.$this->PageNo()),0,1,"R");
 			$this->Line(10,15,200,15);
 			$this->Ln(20); //Blank space for 20
 			}
@@ -77,11 +77,11 @@
 			$this->AddFont('angsa','','angsa.php');
 			$this->SetFont('angsa','',13);
 			$this->SetY(-15);
-	 		//$this->Cell(0,0,iconv( 'UTF-8','TIS-620','test'),0,0,"L");
-			//$this->Cell(-5,0,iconv( 'UTF-8','TIS-620','Date : '.date("Y-m-d")),0,1,"R");
-			$this->Cell(190,0,iconv("UTF-8","TIS-620","ห้องปฏิบัติการวิจัยวิศวกรรมระบบสารสนเทศ คณะวิทยาการสารสนเทศ มหาวิทยาลัยบูรพา "),0,1,'R'); //-5 old value if not have line 2 of right
+	 		//$this->Multicell(0,0,iconv( 'UTF-8','TIS-620','test'),0,0,"L");
+			//$this->Multicell(-5,0,iconv( 'UTF-8','TIS-620','Date : '.date("Y-m-d")),0,1,"R");
+			$this->Multicell(190,0,iconv("UTF-8","TIS-620","ห้องปฏิบัติการวิจัยวิศวกรรมระบบสารสนเทศ คณะวิทยาการสารสนเทศ มหาวิทยาลัยบูรพา "),0,1,'R'); //-5 old value if not have line 2 of right
 			$this->Ln(5);
-			$this->Cell(190,0,iconv("UTF-8","TIS-620","วิทยาลัยการสาธารณสุขสิรินธร จังหวัดขอนแก่น สถาบันพระบรมราชชนก "),0,1,'R');
+			$this->Multicell(190,0,iconv("UTF-8","TIS-620","วิทยาลัยการสาธารณสุขสิรินธร จังหวัดขอนแก่น สถาบันพระบรมราชชนก "),0,1,'R');
 			$this->Line(10,270,200,270);  
 			}
 		}
