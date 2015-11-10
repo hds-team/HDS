2cf32d31f8ac5eed9529c6a59c1715f2c436da28
Joevanjoke <chiichha.vanjoke@gmail.com>
2015-11-10 12:10:09 +0700

    ลองเขียนหน้ารายงาน
    ยังไม่เสร็จ
    จะคุยกับคนอื่นอีกที

---
+++ application/views/HDS/v_chart.php
@@ -0,0 +1,41 @@
+<div class="grid_1">
+    <div class="da-panel"></div>
+</div>
+<div class="grid_2">
+    <div class="da-panel"> 
+      <div class="da-panel-header">
+        <span class="da-panel-title">
+          <img src="<?php echo base_url(); ?>images/icons/black/16/pencil.png" alt="">
+          รายงานสรุป
+        </span>
+      </div> <!-- da-panel-header -->
+      <div class="da-panel-content">
+            <div class="da-form-inline">
+                  <div class="da-form-row">
+                      <label>กราฟในแนวแกน X <span class="required">*</span></label>
+                      <div class="da-form-item large">
+                        
+                      </div>
+                  </div>
+                  <div class="da-form-row">
+                      <label>กราฟในแนวแกน Y <span class="required">*</span></label>
+                      <div class="da-form-item large">
+                        
+                      </div>
+                  </div>
+              </div>
+              <div class="da-button-row">
+                <input type="submit" value="ตกลง" class="da-button green">
+              </div>
+      </div><!-- da-panel-content -->
+    </div><!-- da-panel -->
+  </div>
+
+<div class ="grid_4">
+  <div class ="da-panel-widget">
+    <h1><center>กราฟรายงานสรุปคำร้อง</center></h1>
+      <div id ="da-ex-flot-stacked" style="height: 300px; padding: 0px; position: relative;">
+      
+      </div><!-- da-ex-flot-stacked -->
+  </div><!-- da-panel-widget -->
+</div><!-- grid_4 of chart -->
